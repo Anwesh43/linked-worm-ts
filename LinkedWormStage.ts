250955bfd6a8f5d835cac3ea45ba5c4d336237a6
Anwesh43 <anweshthecool0@gmail.com>
2018-05-12 16:56:51 +0530

    adding LWState class

--- LinkedWormStage.ts
+++ LinkedWormStage.ts
@@ -27,3 +27,34 @@ class LinkedWormStage {
         }
     }
 }
+
+class LWState {
+    scales : Array<number> = [0, 0, 0, 0]
+
+    prevScale : number = 0
+
+    j : number = 0
+
+    dir : number = 0
+
+    update(stopcb : Function) {
+        this.scales[this.j] += 0.1 * this.dir
+        if (Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if (this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir = 0
+                this.prevScale = this.scales[this.j]
+                stopcb()
+            }
+        }
+    }
+
+    startUpdating(startcb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
